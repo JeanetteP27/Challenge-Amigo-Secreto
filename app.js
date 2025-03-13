e20c490c1b6ec672e64a856d3810a506a8a272b9
Jeanette <106854948+JeanetteP27@users.noreply.github.com>
2025-03-13 17:25:03 -0300

    Añadido de comentarios

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const listaAmigos = [];
+const listaAmigos = []; /* Almacena nombres de los amigos*/
 
-const imputAmigo = document.getElementById ("amigo");
+const imputAmigo = document.getElementById ("amigo"); /* Trae valor del amigo actual*/
 const ulListaAmigos = document.getElementById ("listaAmigos");
 const ulResultado = document.getElementById ("resultado");
 
+/* Funcion para agregar amigo a la lista*/
 function agregarAmigo() {
-    if(!imputAmigo.value){
+    if(!imputAmigo.value){ /* En caso de ingresar un valor vacio */
         alert("No puede estar vacio; porfavor ingrese un nombre.");
     }else{
-        if(listaAmigos.includes(imputAmigo.value)){
+        if(listaAmigos.includes(imputAmigo.value)){ /* Comprueba que no se repita el nombre*/
             alert(`El nombre ${imputAmigo.value} ya esta en la lista`);
-        }else{
+        }else{ /* Registra el amigo en la lista*/
             listaAmigos.push(imputAmigo.value);
             ulListaAmigos.innerHTML += `<li>${imputAmigo.value}</li>`;
             imputAmigo.value = "";
@@ -18,12 +19,13 @@ function agregarAmigo() {
     }
 };
 
+/* Funcion para sortear a un amigo de la lista*/
 function sortearAmigo() {
-    if(listaAmigos.length == 0){
-        alert("No hay amigos disponibles.");
-    }else{
+    if(listaAmigos.length == 0){ /* Comprueba que no este vacia la lista*/
+        alert("No hay amigos disponibles; porfavor ingrese un nombre.");
+    }else{ /* Calcula al amigo de manera random*/
         const random = Math.floor(Math.random() * listaAmigos.length);
         const amigoSecreto = listaAmigos[random];
-        ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+        ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`; /* Expresa el resultado*/
     }
 };
